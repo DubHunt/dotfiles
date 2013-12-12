f8d8cdcedf14c54945bca464ca5ccc237c9d2226
Anthony Hunt <anthony.hunt@readytalk.com>
2013-12-11 23:06:45 -0700

    Adding some git aliases

--- home/.aliases.sh
+++ home/.aliases.sh
@@ -1,3 +1,12 @@
+#--Git
 alias g='git'
+alias gl='git log'
+alias gf='git fetch'
+alias gp='git pull'
+alias gs='git status'
+alias gcam='git commit -am'
+alias gwat='git whatchanged'
+alias gwatp='git whatchanged -p'
+
 alias psg='ps -elf | grep -i'
 alias latr='ls -latr'
